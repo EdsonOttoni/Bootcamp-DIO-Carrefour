3877c3e5565dd103976eb1d5bf49157f50b16c01
Edson Ottoni <43525138+EdsonOttoni@users.noreply.github.com>
2021-09-16 16:51:56 -0300

    Update figurinhas.kt

--- 02_dasafios/figurinhas.kt
+++ 02_dasafios/figurinhas.kt
@@ -1,3 +1,7 @@
+// Detalhe
+// Desenvolva um programa capaz de ler um valor inteiro N. N * 2 linhas de saída vão ser apresentadas na execução do programa, 
+// seguindo a lógica do exemplo mais abaixo. Para os valores com mais de seis dígitos, todos os dígitos devem ser apresentados.
+
 fun main(args: Array<String>) {
     val list = mutableListOf<Int>()
 
